b4456b67ea1d85029da592476019a0fdd89dc858
sunlei <973797201@qq.com>
2018-04-16 19:14:49 +0800

    first commit

--- translate/app3.js
+++ translate/app3.js
@@ -1,19 +1,12 @@
-var pinyin = require("pinyin");
-
-function translateToPinyin(str) {
-  let py = pinyin(str, {
-    style: pinyin.STYLE_NORMAL // 设置拼音风格
-  })
-    .map(e => e[0])
-    .join("")
-    console.log(str,"\n",py,"\n")
-    return py
-}
+var OpenCC = require('opencc');
+var opencc = new OpenCC('t2s.json');
+var simpl = opencc.convertSync("漢字");
+console.log(simpl)
 
 var path = require("path");
 var fs = require("fs");
 var rootPath = __filename;
-renameFilesInDir("/Users/sunlei/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9/302e188d912c8a0be03a6a193e184d66/Message/MessageTemp/eadad77e54bcb0c5fa58c2ae0bbf9087/File/切图3/购物车补充6个产品");
+renameFilesInDir("/Users/sunlei/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9/302e188d912c8a0be03a6a193e184d66/Message/MessageTemp/eadad77e54bcb0c5fa58c2ae0bbf9087/File/切图3");
 
 function changeFileName(filepath) {
   fs.stat(filepath, function(err, stats) {
@@ -30,11 +23,9 @@ function changeFileName(filepath) {
       //console.log(thisFilename);
       //这个if就是进行更改文件名的逻辑,可以自行定义,这里定义为将文件命名为当前文件夹的名字加"-文件自身名"
       if (filename != thisFilename && filename.indexOf(parentDirname) < 0) {
-        // var arr = filename.split("_")
-        // if (arr.length > 0) {
-          var newName = translateToPinyin(filename)
+          var newName = opencc.convertSync(filename)
           var newPath = parentDir + "/" + newName;
-          // console.log("going to rename from " + filepath + " to " + newPath);
+          console.log("going to rename from " + filepath + " to " + newPath);
           fs.rename(filepath, newPath);
         // }
       }

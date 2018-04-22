232dd39175a6a215ede8e57f4ce3c08c424f7dd4
sun4076 <973797201@qq.com>
2018-04-22 11:54:27 +0800

    commit

--- test/app.js
+++ test/app.js
@@ -6,11 +6,41 @@ var app = express();
 var publicPath = path.resolve(__dirname, "src"); 
 app.use(express.static(publicPath)); 
 app.get('/data',function(request, response) {
-    let data = fs.readFileSync("./src/data.json")
+    let data = fs.readFileSync("./data.json")
     response.json(JSON.parse(data))
 });
 app.use(function(request, response) {
     response.writeHead(404);
     response.end('404!')
 });
-http.createServer(app).listen(80);
+http.createServer(app).listen(3001);
+
+// 1、接口地址：
+// "http://47.98.133.108/data"
+
+// 2、图片请求方法：
+
+// a、首页：
+// "http://47.98.133.108/index/"+图片名称
+// 比如:
+// "http://47.98.133.108/index/jidiweijinghua.png"
+
+// b、列表:
+// "http://47.98.133.108/list/"+图片名称
+// 比如:
+// "http://47.98.133.108/list/gaobieyouguanghaizaomolizao.png"
+
+// c、详情：
+// "http://47.98.133.108/detail/"+图片名称
+
+// "http://47.98.133.108/detail/jitouxiuhuyanbumijijinghualou_1.jpg"
+
+// d、推荐：
+// "http://47.98.133.108/recommend/"+图片名称
+
+// "http://47.98.133.108/recommend/terunxiuhujitoujinghualou.png"
+
+// e、购物车：
+// "http://47.98.133.108/shopcar/"+图片名称
+
+// "http://47.98.133.108/shopcar/jitouxiuhumianmojingcuimi.png"
--- translate/app2.js
+++ translate/app2.js
@@ -1,28 +1,79 @@
 
 var fs = require('fs');
-var dataJson = "/Users/sunlei/Git/node_demos/test/src/new.json"
+var dataJson = "/Users/sunlei/Git/node_demos/test/src/data.json"
 
 var dataDic = JSON.parse(fs.readFileSync(dataJson));
 
 // // console.log(data1Dic)
-function changeToPy(data) {
-  Object.keys(data.ResultData.ProductList).map(e=>{
-    data.ResultData.ProductList[e] = {
-      ...data.ResultData.ProductList[e],
-      ProductFocus: data.ResultData.ProductList[e].ProductFocus+".png"
+function changeToPy(data, key) {
+  // console.log("changeToPy=------------>\n\n\n\n",data, '  \n\n\n\n')
+  if (Array.isArray(data)) {
+    // console.log("Array.isArray(data))=------------>\n\n\n\n",data, '  \n\n\n\n')
+    return data.map(e => changeToPy(e, key))
+  } else if (typeof (data) == 'object') {
+    // console.log("(typeof (data) == 'object')=------------>\n\n\n\n",data, '  \n\n\n\n')
+    if (data.CatagoryName) {
+      let keys = Object.keys(data)
+      let obj = {}
+      keys.map(e => {
+        data[e] = changeToPy(data[e], data.CatagoryName)
+      })
+      return data
+    } else {
+      if (data.Type == 'Package') {
+        let obj = dataDic.ResultData.PackageList[data.ID]
+        if (key == "首 頁") {
+          obj.indexFocus = "index/" + obj.PackageFocus
+        } else {
+          obj.listFocus = "list/" + obj.PackageFocus
+        }
+        dataDic.ResultData.PackageList[data.ID] = obj
+        obj.ProductID.map(e => {
+          let obj1 = dataDic.ResultData.ProductList[e]
+          obj1.listFocus = "list/" + obj1.ProductFocus
+          obj1.shopCarFocus = "shopcar/" + obj1.ProductFocus
+          dataDic.ResultData.ProductList[e] = obj1
+        })
+      } else if (data.Type == 'Product') {
+        let obj = dataDic.ResultData.ProductList[data.ID]
+        if (key == "首 頁") {
+          obj.indexFocus = "index/" + obj.ProductFocus
+        } else {
+          obj.listFocus = "list/" + obj.ProductFocus
+        }
+        obj.shopCarFocus = "shopcar/" + obj.ProductFocus
+        dataDic.ResultData.ProductList[data.ID] = obj
+      }
+      return data
     }
-  })
-  Object.keys(data.ResultData.PackageList).map(e=>{
-    data.ResultData.PackageList[e] = {
-      ...data.ResultData.PackageList[e],
-      PackageFocus: data.ResultData.PackageList[e].PackageFocus+".png"
-    }
-  })
-  return data
+  } else {
+    return data
+  }
 }
 
 
 
-dataDic = changeToPy(dataDic)
+changeToPy(dataDic.ResultData.DataList)
 
-fs.writeFileSync("/Users/sunlei/Git/node_demos/test/src/data.json", JSON.stringify(dataDic));
+
+Object.keys(dataDic.ResultData.ProductList).map(e=>{
+  let obj = dataDic.ResultData.ProductList[e]
+  console.log('obj.ProductContent===',obj.ProductContent)
+  if (obj.ProductContent) {
+    obj.ProductContent = obj.ProductContent.map(f=>{
+      return "detail/"+f
+    })
+  }
+  if (obj.ProductRecommendation) {
+    obj.ProductRecommendation.map(f=>{
+      let tempobj = dataDic.ResultData.ProductList[f]
+      if (!tempobj.recommendFocus || tempobj.recommendFocus.indexOf('recommend/') == -1) {
+        tempobj.recommendFocus = "recommend/"+tempobj.ProductFocus
+      }
+      
+      dataDic.ResultData.ProductList[f] = tempobj
+    })
+  }
+  dataDic.ResultData.ProductList[e] = obj
+})
+fs.writeFileSync("/Users/sunlei/Git/node_demos/test/data2.json", JSON.stringify(dataDic));
